8b28d204f8555eedbe41128c30709883b69cebe1
Shivanik05-ui <keerthishivani0045@gmail.com>
2025-08-02 11:15:27 +0530

    Update IncrementDecrement.java

--- IncrementDecrement.java
+++ IncrementDecrement.java
@@ -5,13 +5,12 @@ public class IncrementDecrement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 int x = 10;
-	        System.out.println("Original x = " + x);
-	        System.out.println("x++ = " + (x++)); 
-	        System.out.println("After x++ x = " + x);
-	        System.out.println("++x = " + (++x)); 
-	        System.out.println("x-- = " + (x--)); 
-	        System.out.println("After x-- x = " + x);
-	        System.out.println("--x = " + (--x)); 
+	       
+	        System.out.println(x++); 
+	        System.out.println(++x); 
+	        System.out.println(x--); 
+	        System.out.println(--x); 
 	}
 
 }
+
